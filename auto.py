79c0855f92afd946b19d97465c66bfad39ba1693
Rea <lathindu2000@gmail.com>
2021-02-11 20:26:53 +0530

    Automation Script Compleated[The serial code finder of the mg link can still be optimized ]

--- auto.py
+++ auto.py
@@ -2,71 +2,121 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 
 
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+
+requiredmovielink = input('Enter the movie link:')
+
+
+driver = webdriver.Chrome(
+    options=options, executable_path=r'C:\WebDrivers\chromedriver.exe')
+driver.get(requiredmovielink)
+
+
+# Function Definitions
+
 def findmegalink():
     megalinks = driver.find_elements_by_class_name("shortc-button")
     required = megalinks[10].get_attribute('outerHTML')
 
-    linkmod1 = required[:16]
+    linkmod1 = required[:17]
     linkmod2 = linkmod1[1:]
+    linkmod2.strip()
 
-    actuallink = driver.find_element_by_xpath(
-        '//*[@id="the-post"]/div/div[2]/div[2]/div/'+linkmod2 + '[10]')
+    onlymegalinks = driver.find_elements_by_xpath(
+        '//*[@id="the-post"]/div/div[2]/div[2]/div//'+linkmod2 + "[contains(., 'MG')]")
 
-    return actuallink
-
-
-driver = webdriver.Chrome()
-
-driver.get('https://pahe.ph')
-
-searchbox = driver.find_element_by_xpath('//*[@id="s-header"]')
-searchbox.send_keys('the little')
-
-searchButton = driver.find_element_by_xpath(
-    '//*[@id="searchform-header"]/button')
-
-searchButton.click()
-
-driver.implicitly_wait(5)
-
-driver.switch_to.window(driver.window_handles[1])
-driver.close()
-
-driver.switch_to.window(driver.window_handles[0])
+    return onlymegalinks
 
 
-movielink = driver.find_element_by_xpath(
-    '//*[@id="main-content"]/div[1]/div[2]/div/div/ul[1]/li/div[1]/h2/a')
+def findavalableresolutions():
+    print('Searching For Avalable Resolutions....')
+    strongscount = len(driver.find_elements_by_xpath(
+        '//*[@id="the-post"]/div/div[2]/div[2]/div/strong'))
 
-movielink.click()
+    allstrongs = driver.find_elements_by_xpath(
+        '//*[@id="the-post"]/div/div[2]/div[2]/div/strong')
+
+    resolutionsettings = []
+
+    for res in allstrongs:
+        text = res.text
+
+        resolutionsettings.append(text)
+
+    # testing
+    # for i in resolutionsettings:
+    #     print(i)
+
+    # print(strongscount)
+    # print(type(resolutionsettings))
+    # print('---------------------')
+    # print(resolutionsettings[2])
+    return resolutionsettings
 
 
-findmegalink().click()
+def skipagreement():
+    agreement = driver.find_element_by_xpath(
+        '//*[@id="qc-cmp2-ui"]/div[2]/div/button[1]')
+
+    agreement.click()
+
+
+def skipintercelestial():
+    print('Skipping Intercelestial.....')
+    firstverfication = driver.find_element_by_xpath('//*[@id="landing"]')
+
+    firstverfication.submit()
+
+    driver.implicitly_wait(5)
+
+    generatelink = driver.find_element_by_xpath('//*[@id="generater"]')
+
+    generatelink.click()
+
+    driver.implicitly_wait(6)
+
+    downloadlink = driver.find_element_by_xpath('//*[@id="showlink"]')
+
+    downloadlink.click()
+
+
+def skiplinegee():
+    print('Finalizing....')
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    finallink = driver.find_element_by_xpath(
+        '//*[@id="content"]/div/div/div[1]/div/div[1]/div[3]/center/a')
+
+    finallink.click()
+    megaurl = driver.current_url
+    print('Requested Mega Url:  '+megaurl)
+
+
+resolutions = findavalableresolutions()
+
+for index, item in enumerate(resolutions):
+    print(item+'--------'+str(index+1))
+
+
+requestedreso = input(
+    "Enter The Number Assigned To The  Required Resolution: ")
+
+
+rightlink = findmegalink()
+
+rightlink[int(requestedreso)-1].click()
 
 
 driver.implicitly_wait(10)
 
-agreement = driver.find_element_by_xpath(
-    '//*[@id="qc-cmp2-ui"]/div[2]/div/button[1]')
 
-agreement.click()
+skipagreement()
 
-firstverfication = driver.find_element_by_xpath('//*[@id="landing"]')
+skipintercelestial()
 
-firstverfication.submit()
-
-
-driver.implicitly_wait(5)
-
-generatelink = driver.find_element_by_xpath('//*[@id="generater"]')
-
-generatelink.click()
-
-driver.implicitly_wait(6)
-
-downloadlink = driver.find_element_by_xpath('//*[@id="showlink"]')
-
-downloadlink.click()
 
 driver.implicitly_wait(6)
 
@@ -74,10 +124,5 @@ driver.implicitly_wait(6)
 driver.switch_to.window(driver.window_handles[1])
 driver.implicitly_wait(6)
 
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-finallink = driver.find_element_by_xpath(
-    '//*[@id="content"]/div/div/div[1]/div/div[1]/div[3]/center/a')
-
-
-finallink.click()
+skiplinegee()
--- testdriver.py
+++ testdriver.py
@@ -3,44 +3,92 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 # driverlocation = 'C:\\\\ProgramData\chocolatey\bin\chromedriver.exe'
 options = webdriver.ChromeOptions()
-driver = webdriver.Chrome()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+# driver = webdriver.Chrome()
 
-driver.get('https://pahe.ph')
+# driver.get('https://pahe.ph')
 
-searchbox = driver.find_element_by_xpath('//*[@id="s-header"]')
-searchbox.send_keys('the little')
+# searchbox = driver.find_element_by_xpath('//*[@id="s-header"]')
+# searchbox.send_keys('the little')
 
-searchButton = driver.find_element_by_xpath(
-    '//*[@id="searchform-header"]/button')
-searchButton.click()
-
-
-movielink = driver.find_element_by_xpath(
-    '//*[@id="main-content"]/div[1]/div[2]/div/div/ul[1]/li/div[1]/h2/a')
+# searchButton = driver.find_element_by_xpath(
+#     '//*[@id="searchform-header"]/button')
+# searchButton.click()
 
 
-movielink.click()
+# movielink = driver.find_element_by_xpath(
+#     '//*[@id="main-content"]/div[1]/div[2]/div/div/ul[1]/li/div[1]/h2/a')
 
 
-# megalink = driver.find_element_by_xpath(
-#     '//*[@id="the-post"]/div/div[2]/div[2]/div/name()')
+# movielink.click()
 
-driver.implicitly_wait(5)
+
+# # megalink = driver.find_element_by_xpath(
+# #     '//*[@id="the-post"]/div/div[2]/div[2]/div/name()')
+
+# driver.implicitly_wait(5)
+
+requiredmovielink = input('Enter the movie link:')
+
+
+driver = webdriver.Chrome(
+    options=options, executable_path=r'C:\WebDrivers\chromedriver.exe')
+driver.get(requiredmovielink)
 
 
 def findmegalink():
     megalinks = driver.find_elements_by_class_name("shortc-button")
     required = megalinks[10].get_attribute('outerHTML')
-   
 
-    linkmod1 = required[:16]
+    linkmod1 = required[:17]
     linkmod2 = linkmod1[1:]
+    linkmod2.strip()
+
+    onlymegalinks = driver.find_elements_by_xpath(
+        '//*[@id="the-post"]/div/div[2]/div[2]/div//'+linkmod2 + "[contains(., 'MG')]")
+
+    # for link in onlymegalinks:
+    #     i = link.get_attribute('outerHTML')
+    #     print(i)
+
+    return onlymegalinks
 
 
-    actuallink = driver.find_element_by_xpath(
-        '//*[@id="the-post"]/div/div[2]/div[2]/div/'+linkmod2 + '[10]')
+def findavalableresolutions():
+    strongscount = len(driver.find_elements_by_xpath(
+        '//*[@id="the-post"]/div/div[2]/div[2]/div/strong'))
 
-    return actuallink
+    allstrongs = driver.find_elements_by_xpath(
+        '//*[@id="the-post"]/div/div[2]/div[2]/div/strong')
+
+    resolutionsettings = []
+
+    for res in allstrongs:
+        text = res.text
+
+        resolutionsettings.append(text)
+
+    # testing
+    # for i in resolutionsettings:
+    #     print(i)
+
+    # print(strongscount)
+    # print(type(resolutionsettings))
+    # print('---------------------')
+    # print(resolutionsettings[2])
+    return resolutionsettings
 
 
-findmegalink().click()
+resolutions = findavalableresolutions()
+
+for index, item in enumerate(resolutions):
+    print(item+'--------'+str(index+1))
+
+
+requestedreso = input(
+    "Enter The Number Assigned To The  Required Resolution: ")
+
+
+rightlink = findmegalink()
+
+rightlink[int(requestedreso)-1].click()
